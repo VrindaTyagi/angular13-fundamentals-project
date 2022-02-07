a4cdbbb9b2da57947dd084fd6d9c525a68b9bb35
Vrinda Tyagi <vrindatyagi09@gmail.com>
2022-02-07 17:22:41 +0530

    feat: Route Params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { AdminPayrollComponent } from './examples/child-routes/admin-payroll/adm
 import { AdminVacationComponent } from './examples/child-routes/admin-vacation/admin-vacation.component';
 import { AdminComponent } from './examples/child-routes/admin.component';
 import { ExamplesComponent } from './examples/examples.component';
+import { ProtectedComponent } from './examples/protected/protected.component';
+import { RouteParamsComponent } from './examples/route-params/route-params.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -22,6 +24,10 @@ const routes: Routes = [
           { path: 'vacation', component: AdminVacationComponent },
         ],
       },
+      { path: 'params', component: RouteParamsComponent },
+      { path: 'params/:id', component: RouteParamsComponent },
+      { path: 'protected', component: ProtectedComponent},
+
     ],
   },
   { path: '**', redirectTo: '/home' },
